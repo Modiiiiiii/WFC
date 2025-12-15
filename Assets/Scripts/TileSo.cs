47e0c223244c012ab763f4badb65a454c6f4730c
Modi <modi.xiong@optillusion.games>
2025-12-15 21:10:51 +0800

    wfc

--- Assets/Scripts/TileSo.cs
+++ Assets/Scripts/TileSo.cs
@@ -13,8 +13,9 @@ public class TileSo : ScriptableObject
     // 连接规则：上、右、下、左
     public TileType[] upConnections;
     public TileType[] downConnections;
-    public TileType[] rightConnections;
     public TileType[] leftConnections;
+    public TileType[] rightConnections;
+ 
     
     // 检查是否可以连接
     public bool CanConnectTo(TileType otherTile, int direction)
