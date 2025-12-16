bfd97f058a9f79c45633d8c3a764f0553a114081
Modi <modi.xiong@optillusion.games>
2025-12-16 11:52:19 +0800

    wfc

--- Assets/Scripts/TileSo.cs
+++ Assets/Scripts/TileSo.cs
@@ -1,5 +1,6 @@
 using System;
 using UnityEngine;
+using UnityEngine.Serialization;
 using UnityEngine.UI;
 
 [CreateAssetMenu(fileName = "TileData", menuName = "WFC/TileData")]
@@ -7,5 +8,21 @@ public class TileSo : ScriptableObject
 {
     public TileType tileType;
     //public Sprite sprite;
-    public Seam[] allConnections;//上右下左 顺时针 
+    public Seam[] AllConnections
+    {
+        get
+        {
+            return new[]
+            {
+                up,
+                right,
+                down,
+                left
+            };
+        }
+    } //上右下左 顺时针 
+    public Seam up;
+    public Seam right;
+    public Seam down;
+    public Seam left;
 }
